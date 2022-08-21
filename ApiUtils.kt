56d11b5d553050eab719aa7121adcc3472c2625a
Burak Aydin <90382482+GrgnPl@users.noreply.github.com>
2022-08-21 20:45:58 +0300

    Update ApiUtils.kt

--- ApiUtils.kt
+++ ApiUtils.kt
@@ -7,12 +7,11 @@ class ApiUtils {
     @Keep
     companion object //Bu fonksiyonla interface'e erişim sağlıyoruz.
     {
-    val BASE_URL = "https://siberguvenliklisesi.com/ctfapp/"
+    val BASE_URL = "/" Domain Adresiniz
 
     fun getKisilerDaoInterface():KisilerDaoInterface
     {
-    return RetrofitClient.getClient(BASE_URL).create(KisilerDaoInterface::class.java) //Static olan Retrofit Client sınıfındaki getClient Fonksiyonunu çağırdık, yukarıda oluşturmuş olduğumuz Baseurl'i mizi de atadık.
-    }
+    return RetrofitClient.getClient(BASE_URL).create(KisilerDaoInterface::class.java)
 
     }
 }
